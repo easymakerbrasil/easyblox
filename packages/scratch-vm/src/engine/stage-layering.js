fa1b13c5e8b91ce8b1da13edb8f94be98ad53ca6
easymakerbrasil <easymakerbrasil@gmail.com>
2026-09-23 17:24:03 -0300

    feat: render EasyBlox QR overlay on Stage

--- packages/scratch-vm/src/engine/stage-layering.js
+++ packages/scratch-vm/src/engine/stage-layering.js
@@ -15,13 +15,18 @@ class StageLayering {
         return 'sprite';
     }
 
+    static get EASYBLOX_QR_LAYER () {
+        return 'easybloxQr';
+    }
+
     // Order of layer groups relative to each other,
     static get LAYER_GROUPS () {
         return [
             StageLayering.BACKGROUND_LAYER,
             StageLayering.VIDEO_LAYER,
             StageLayering.PEN_LAYER,
-            StageLayering.SPRITE_LAYER
+            StageLayering.SPRITE_LAYER,
+            StageLayering.EASYBLOX_QR_LAYER
         ];
     }
 }
